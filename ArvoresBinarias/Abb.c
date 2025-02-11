ee3cc9daa0b1e54638d51b52353cf1d2b30fbe1b
CarlosDiasS <139489227+CarlosDiasS@users.noreply.github.com>
2025-02-11 16:42:38 -0300

    Funções Lista 8 restantes

--- ArvoresBinarias/Abb.c
+++ ArvoresBinarias/Abb.c
@@ -353,12 +353,116 @@ int diffProfundidade(int a, int b, Arvore *arvore){
     //chamar profundidadeNo para cada um
     // if comparando qual e maior e subtraindo o menor 
 
+    int temp1 = profundidadeNo(a,arvore);
+    int temp2 = profundidadeNo(b, arvore);
+    int result = 0;
+    if(temp1>temp2){
+        result = temp1 - temp2;
+    }
+    else{
+        result = temp2  - temp1;
+    }
+    return result;
+}
+
+/*
+    Ex. 4 Ancestral Comum mais baixo entre dois nos 
+    -> recursao
+*/
+No *ancestralComum(int a, int b, No *no){
+
+    if(no ==NULL) return NULL;
+
+    if(no->valor == a || no->valor == b) return no;
+
+    No *resEsq = ancestralComum(a,b, no->esq);
+    No *resDir = ancestralComum(a,b, no->dir);
+
+    //se ambas as chamadas recursivas retornaram um no,
+    //significa que o no informado é o ancestral comum
+    //Pois dele surgem no->dir e no->esq 
+    if(resEsq !=NULL && resDir != NULL) return no;
+    
+    if(resEsq == NULL){
+        return ancestralComum(a,b,no->dir);
+    }
+    else{
+        return ancestralComum(a,b,no->esq);
+    }
+}
+
+/*
+    Ex. 5 Distancia entre dois nos da arvore
+    -> soma a distancia ate o anc comum dos dois nos
+    -> garante o menor caminho, por conta do anc comum  
+*/
+int distanciaNos(int a, int b, Arvore *arvore){
+
+    No *anc = ancestralComum(a,b,arvore->raiz);
+
+    if(!anc) return -1;
+
+    int distanciaX = diffProfundidade(a, anc->valor,arvore);
+    int distanciaY = diffProfundidade(b,anc->valor,arvore);
+    return distanciaX + distanciaY;
+
+}
+/*
+    Ex. 6 
+    -> um simples for gera uma arvore degenarada(semelhante à lista encadeada)
+    ->tentativa recursiva O(log N)
+*/
+//deve receber um vetor ordenado 
+//rever
+void vetorToABB(int *vet,int inicio, int fim, Arvore *arvore){
+
+    // for(int i=0;i<size;i++){
+    //     insereAbb(vet[i],arvore);
+    // }
+    //caso base : vetor inserido totalmente
+    if(inicio>fim) return;
+
+    int meio = (inicio+fim)/2;
+
+    insereAbb(vet[meio],arvore);
+
+    //subvetor a esquerda
+    vetorToABB(vet,inicio,meio-1,arvore);
+    //subvetor a direita
+    vetorToABB(vet,meio+1,fim,arvore);
+}
+
+/*
+    Ex. 7 Arvore binaria 
+*/
+int isBinaria(No *aux){
+
+    //caso base
+    if(aux == NULL) return 1;
+    //caso base
+    if(aux->esq == NULL && aux->dir == NULL) return 1;
+    //verificacao da condicao binaria
+    if(aux->esq != NULL && aux->dir != NULL){
+        if(aux->valor == (aux->esq->valor + aux->dir->valor)) {
+            //sub-arvores a esquerda e direita 
+            return isBinaria(aux->dir) && isBinaria(aux->esq);
+        }
+    }
+    return 0;
+}
+
+void insereArvoreBinaria(int n, Arvore* Arvore){
+}
+
+/*
+    Ex. 8 Somar todos os valores de uma ABB
+*/
+int somaValoresAb(Arvore *arvore){
 
 
 
 
 }
-
 
 int main()
 {
@@ -384,10 +488,37 @@ int main()
     printf("\n");
     printf("\n");
     imprimeFormatado(arvore);
+    printf("\n");
     int cont = 0;
     numFolhas(arvore->raiz,&cont);
     printf("Quantidade de folhas: %d\n", cont);
     printf("Profundidade do no 13: %d\n",profundidadeNo(13,arvore));
+    printf("Diferenca de profundidade entre 18 e 13: %d\n",diffProfundidade(18,13,arvore));
+    printf("Diferenca de profundidade entre 12 e 13: %d\n",diffProfundidade(12,13,arvore));
+    No *anc = ancestralComum(13,19,arvore->raiz);
+    printf("Ancestral comum entre 13 e 19: %d\n",anc->valor);
+    printf("Distancia relativa entre 2 e 13: %d\n",distanciaNos(2,13,arvore));
+
+    int vet[] = {1,2,3,4,5,6,7,8,9};
+    int n = sizeof(vet)/sizeof(vet[0]);
+    printf("%d\n", n);
+    Arvore *abb = (Arvore*)malloc(sizeof(Arvore));
+    abb->raiz = NULL;
+    vetorToABB(vet,0,n,arvore);
+    printf("\n");
+    percursoEmOrdem(abb->raiz);
+    imprimeFormatado(abb);
+    imprimeLinhaLinha(abb->raiz);
+
+    Arvore *bin = (Arvore*)malloc(sizeof(Arvore));
+    bin->raiz = NULL;
+    insereArvoreBinaria(10,bin);
+    insereArvoreBinaria(4,bin);
+    insereArvoreBinaria(6,bin);
+    insereArvoreBinaria(3,bin);
+    insereArvoreBinaria(3,bin);
+    insereArvoreBinaria(2,bin);
+    insereArvoreBinaria(2,bin);
 
     return 0;
 }
